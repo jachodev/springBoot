83633e32c1a157f985065de64d0d0521b6da7c4d
jacho <javier.huaman.huayllani@gmail.com>
2022-08-25 20:30:49 -0500

    join example

---
+++ join-example/src/main/java/com/roytuts/spring/data/jpa/left/right/inner/cross/join/JoinExampleApplication.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.data.jpa.left.right.inner.cross.join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinExampleApplication.class, args);
+	}
+
+}
---
+++ join-example/src/test/java/com/roytuts/spring/data/jpa/left/right/inner/cross/join/JoinExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.data.jpa.left.right.inner.cross.join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
